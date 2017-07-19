3e2f5dcecf5b6a816e6ff064ed7f06a4eb69da00
JayIm <steveim1201@gmail.com>
2017-07-20 01:06:11 +0900

    ProjectAddController 및 ProjectAddForm추가

--- src/spms/dao/MySqlProjectDao.java
+++ src/spms/dao/MySqlProjectDao.java
@@ -1,6 +1,7 @@
 package spms.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -9,7 +10,6 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import spms.annotation.Component;
-import spms.vo.Member;
 import spms.vo.Project;
 
 @Component("projectDao")
@@ -52,5 +52,34 @@ public class MySqlProjectDao implements ProjectDao {
 	    	 try {if (stmt != null) stmt.close();} catch(Exception e) {}
 	    	 try {if (connection != null) connection.close();} catch(Exception e) {}
 	     }
+	}
+	
+	public int insert(Project project) throws Exception {
+		Connection connection = null;
+		PreparedStatement stmt = null;
+		
+		try {
+			connection = ds.getConnection();
+			stmt = connection.prepareStatement(
+					  "INSERT INTO PROJECTS"
+					+ "(PNAME, CONTENT, STA_DATE, END_DATE, STATE, CRE_DATE, TAGS)"
+					+ " VALUES (?, ?, ?, ?, 0, NOW(), ?)"
+					);
+			stmt.setString(1, project.getTitle());
+			stmt.setString(2, project.getContent());
+			stmt.setDate(3, new java.sql.Date(project.getStartDate().getTime()));
+			stmt.setDate(4, new java.sql.Date(project.getEndDate().getTime()));
+			stmt.setString(5, project.getTags());
+			
+			return stmt.executeUpdate();
+			
+		} catch (Exception e) {
+			throw e;
+			
+		} finally {
+			try {if (stmt != null) stmt.close();} catch(Exception e) {}
+	    	 	try {if (connection != null) connection.close();} catch(Exception e) {}
+		}
+		
 	}
 }
